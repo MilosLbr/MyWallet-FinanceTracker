b81648a22fdcef5df2ce787e2ea072da9b4a56f7
MilosLbr <brankovicmilos@hotmail.com>
2020-02-17 21:38:16 +0100

    Add the angular SPA

---
+++ MyWallet-SPA/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'MyWallet-SPA';
+
+  ngOnInit(){
+    const baseUrl = environment.apiUrl;
+    
+  }
+
+}
